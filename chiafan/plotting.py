954aae335369f040f6fb49778f476ba00b66d1a7
Break Yang <breakds@gmail.com>
2021-05-10 10:24:53 -0700

    Remove -it for docker command

--- chiafan/plotting.py
+++ chiafan/plotting.py
@@ -75,13 +75,13 @@ class PlottingProcess(object):
     def _create_plotting_job(self):
         if not PlottingProcess._wait_for_chiabox_docker(20):
             return None
-        subprocess.run(['docker', 'exec', '-it', 'chiabox',
+        subprocess.run(['docker', 'exec', 'chiabox',
                         'mkdir', '-p', f'{self.plotting_space}'])
-        subprocess.run(['docker', 'exec', '-it', 'chiabox',
+        subprocess.run(['docker', 'exec', 'chiabox',
                         'mkdir', '-p', f'{self.destination}'])
         self.log_file = open(self.log_path, 'w')
         proc = subprocess.Popen([
-            'docker', 'exec', '-it', 'chiabox', 'venv/bin/chia'
+            'docker', 'exec', 'chiabox', 'venv/bin/chia'
             'plots', 'create',
             '-t', f'{self.plotting_space}',
             '-d', f'{self.destination}',
