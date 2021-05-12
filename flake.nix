620e8f287b2d85451d935387a84940f736e04f8a
Break Yang <breakds@gmail.com>
2021-05-12 07:33:05 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -23,26 +23,6 @@
         name = "chiafan-dev";
         buildInputs = with pkgs; [
           chiafan-py-dev
-          (pkgs.writeCBin "chiaplot1" ''
-             #include <stdio.h>
-             #include <unistd.h>
-             #include <time.h>
-             int main(int argc, char **argv) {
-               FILE *f = fopen("/home/breakds/Downloads/20210508_23_12_27.log", "r");
-               char *line;
-               size_t len = 0;
-               ssize_t read;
-               struct timespec ts;
-               ts.tv_sec = 0;
-               ts.tv_nsec = 200000000;  // 200 ms
-               while ((read = getline(&line, &len, f)) != -1) {
-                 printf("%s\n", line);
-                 nanosleep(&ts, NULL);
-               }
-               fclose(f);
-               return 0;
-             }
-           '')
         ];
       };
     });
